57c48370c163209bd2a818c1d64110d8eb82d0ff
dominic <dominicrawlins98@gmail.com>
2019-04-15 12:41:33 +0100

    navbar fixed

--- client/src/components/Toolbar.vue
+++ client/src/components/Toolbar.vue
@@ -30,6 +30,7 @@
     <router-link :to="{path: '/index'}">
   <li class="toolbarItem">Home</li>
 </router-link>
+<li class="toolbarItem">
   <div class="dropdown">
     <button class="dropbtn">Teams
       <i class="fa fa-caret-down"></i>
@@ -101,8 +102,10 @@
         </router-link>
     </div>
   </div>
+
   </div>
   </div>
+</li>
 </div>
 
 
