8308bed82f0bcb103fb0222e95fb1fa26b130915
dominic <dominicrawlins98@gmail.com>
2019-05-26 00:56:53 +0100

    input bar chart which grows

--- site/client/src/components/scripts/topTeamStats.js
+++ site/client/src/components/scripts/topTeamStats.js
@@ -1,13 +1,17 @@
 import IndiTopStats from '@/components/IndiTopStats'
+import BarChart from '@/components/BarChart'
 export default {
   components: {
-    IndiTopStats
+    IndiTopStats,
+    BarChart
   },
     data () {
     return{
       stat: "goalsAgainst",
       columns: ["name", "goalsFor", "goalsAgainst"],
-      order: "desc"
+      order: "desc",
+      heights: [50, 20, 10, 30, 50],
+      maxLength: 200
     }
 
   },
