a1607256e70b8d798ef90e8db27dbbd4ff26432e
dominic <dominicrawlins98@gmail.com>
2019-05-26 21:55:01 +0100

    bcrypt working

--- site/server/src/controllers/AuthenticationController.js
+++ site/server/src/controllers/AuthenticationController.js
@@ -4,6 +4,7 @@ const path = require('path')
 const dbPath = path.join(__dirname, '../db/', 'footballStats.db')
 const jwt = require('jsonwebtoken')
 const config = require('../config/config')
+const bcrypt = require('bcrypt')
 
 function jwtSignUser(user) {
   const ONE_WEEK = 60 * 60 * 24 * 7
@@ -20,20 +21,23 @@ module.exports = {
           console.error(err.message);
         }
       })
-      const sqlQuery = 'INSERT INTO users (email, password) VALUES (?, ?)'
-      const params =  [req.body.email, req.body.password]
-      db.run(sqlQuery, params, function (err) {
-        if (err) {
-          console.log('Error running sql ' + sql)
+      bcrypt.hash(req.body.password, 10, function(err, hash){
+        if(err){
           console.log(err)
-          //reject(err)
-        } else {
-          console.log(db)
-          console.log("run database command")
-          //resolve({ id: this.lastID })
         }
+        const sqlQuery = 'INSERT INTO users (email, password) VALUES (?, ?)'
+        const params =  [req.body.email, hash]
+        db.run(sqlQuery, params, function (err) {
+          if (err) {
+            console.log('Error running sql ' + sql)
+            console.log(err)
+          } else {
+            console.log(db)
+            console.log("run database command")
+          }
+        })
+        db.close()
       })
-      db.close()
     } catch(err){
       res.status(400).send({
         error: err.message
@@ -49,29 +53,32 @@ module.exports = {
           console.error(err.message);
         }
       })
-      const query = 'SELECT * FROM users WHERE (email, password) = (?, ?)'
-      const parameters = [req.body.email, req.body.password]
+      const query = 'SELECT * FROM users WHERE email =  ?'
+      const parameters = [req.body.email]
       db.all(query, parameters, (err, rows) => {
         if(err) {
           console.log('error running sql')
         }
-        // console.log(rows);
-        const allEmails = rows.map(e => e.email);
-        const allPasswords = rows.map(p => p.password)
-        //console.log(allEmails)
-        //console.log(allPasswords)
-        if(allEmails.length === 0) {
+        if(rows.length === 0) {
           //console.log('login invalid')
           res.status(403).send({
-            error: 'The login information was incorrect'
+            error: 'The username was incorrect'
           })
         } else {
+          bcrypt.compare(req.body.password, rows[0].password, (err, isMatch) => {
+            if(err){
+              console.error(err.message)
+            }
+            if(isMatch){
+              res.send({
+                token: jwtSignUser(rows[0])
+
+              })
+            }
+          })
           //const userJson = rows.toJSON()
           //userJson = JSON.stringify(rows[0])
-          res.send({
-            token: jwtSignUser(rows[0])
 
-          })
         }
       })
       db.close()
