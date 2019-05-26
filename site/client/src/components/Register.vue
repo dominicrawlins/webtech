6590cf02f6063b81c2d524d1df9d71c94148b0de
dominic <dominicrawlins98@gmail.com>
2019-05-26 23:17:07 +0100

    only register with correct types

--- site/client/src/components/Register.vue
+++ site/client/src/components/Register.vue
@@ -1,31 +1,45 @@
 <template>
   <div class="center">
-        <div class="log-box">
-    <h1>
+    <div class="log-box">
+      <h1>
+        Register
+      </h1>
+      <p>Please enter your email address and choose a password to register</p>
+
+
+        <div class="tickorcross">
+          <img src="../assets/tick.png" height="20" v-if="emailChecked && emailCorrect"/>
+          <img src="../assets/cross.png" height="18" v-if="emailChecked && !emailCorrect"/>
+          </div>
+        <input
+        type="email"
+        name="email"
+        v-model="email"
+        placeholder="email" @change="checkEmail()" class="inlineRegister"/>
+
+
+      <br>
+      <div class="tickorcross">
+        <img src="../assets/tick.png" height="20" v-if="passwordChecked && passwordCorrect"/>
+        <img src="../assets/cross.png" height="18" v-if="passwordChecked && !passwordCorrect"/>
+        </div>
+
+      <input
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="password" @change="checkPassword()" class="inlineRegister"/>
+      <br>
+      <p class="inputCheck" v-bind:style="{color: longColour}"> Password must be at least 8 characters long </p>
+      <p class="inputCheck" v-bind:style="{color: upperColour}"> Password must contain an uppercase character</p>
+      <p class="inputCheck" v-bind:style="{color: numberColour}"> Password must contain a number</p>
+      <div class="error" v-html="error" />
+      <br>
+      <button
+      @click="register" v-if="passwordCorrect & emailCorrect">
       Register
-    </h1>
-    <p>Please enter your email address and choose a password to register</p>
-
-
-    <input
-    type="email"
-    name="email"
-    v-model="email"
-    placeholder="email" />
-    <br>
-    <input
-    type="password"
-    name="password"
-    v-model="password"
-    placeholder="password" />
-    <br>
-    <div class="error" v-html="error" />
-    <br>
-    <button
-    @click="register">
-    Register
-  </button>
-</div>
+    </button>
+  </div>
 </div>
 </template>
 
--- site/client/src/components/scripts/register.js
+++ site/client/src/components/scripts/register.js
@@ -4,7 +4,14 @@ export default {
     return{
       email: '',
       password: '',
-      error: null
+      error: null,
+      emailCorrect: false,
+      emailChecked: false,
+      passwordCorrect: false,
+      passwordChecked: false,
+      longColour: 'grey',
+      upperColour: 'grey',
+      numberColour: 'grey'
     }
 
   },
@@ -20,7 +27,45 @@ export default {
       }catch(err){
         this.error = err.response.data.error
       }
+    },
+    checkEmail(){
+      this.emailChecked = true
+      var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      this.emailCorrect = re.test(this.email)
+    },
+    checkPassword(){
+      this.passwordChecked = true
+      this.passwordCorrect = true
+      if(this.passwordLongEnough()){
+        this.longColour = 'black'
+      }
+      else{
+        this.longColour = 'red'
+        this.passwordCorrect = false
+      }
+      if(this.passwordUppercase()){
+        this.upperColour = 'black'
+      }
+      else{
+        this.upperColour = 'red'
+        this.passwordCorrect = false
+      }
+      if(this.passwordNumber()){
+        this.numberColour = 'black'
+      }
+      else{
+        this.numberColour = 'red'
+        this.passwordCorrect = false
+      }
+    },
+    passwordLongEnough(){
+      return(this.password.length >= 8)
+    },
+    passwordUppercase(){
+      return(/[A-Z]/.test(this.password))
+    },
+    passwordNumber(){
+      return(/[0-9]/.test(this.password))
     }
-
   }
 }
